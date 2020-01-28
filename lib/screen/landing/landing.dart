d916ed3166ee74d86c2b9e6e951c9836ab92ea20
Edouard Andrei <andrei.edouard95@gmail.com>
2020-01-28 00:47:24 +0000

    Changed some radio stations links

--- lib/screen/landing/landing.dart
+++ lib/screen/landing/landing.dart
@@ -16,9 +16,9 @@ class _LandingState extends State<Landing> {
     Station('assets/images/stations/profm.png', 'Pro FM', 'http://edge126.rdsnet.ro:84/profm/profm.mp3'),
     Station('assets/images/stations/radiozu.png', 'Radio ZU', 'https://live2ro.antenaplay.ro/radiozu/radiozu-48000.m3u8'),
     Station('assets/images/stations/magicfm.png', 'Magic FM', 'http://live.magicfm.ro:9128/magicfm.aacp'),
-    Station('assets/images/stations/virgin.png', 'Virgin', 'http://astreaming.virginradio.ro:8000/virgin_aacp_64k.m3u'),
+    Station('assets/images/stations/virgin.png', 'Virgin', 'http://astreaming.virginradio.ro:8000/virgin_aacp_64k'),
     Station('assets/images/stations/onefm.png', 'One FM', 'http://live.onefm.ro:9128/onefm.aacp'),
-    Station('assets/images/stations/vibefm.png', 'Vibe FM', 'http://astreaming.vibefm.ro:8000/vibefm_aacp48k.m3u'),
+    Station('assets/images/stations/vibefm.png', 'Vibe FM', 'http://astreaming.vibefm.ro:8000/vibefm_aacp48k'),
     Station('assets/images/stations/rockfm.png', 'Rock FM', 'http://live.rockfm.ro:9128/rockfm.aacp')
   ];
 
